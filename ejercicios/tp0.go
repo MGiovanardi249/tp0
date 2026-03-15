0c33b60c9b9fe967151c125675bb90bb372e3a88
Maximo Giovanardi <mgiovanardi@fi.uba.ar>
2026-03-15 08:16:26 -0300

    Implemente la funcion comparar <mgiovanardi>

--- ejercicios/tp0.go
+++ ejercicios/tp0.go
@@ -2,7 +2,9 @@ package ejercicios
 
 // Swap intercambia dos valores enteros.
 func Swap(x *int, y *int) {
-
+	auxiliar := *x
+	*x = *y
+	*y = auxiliar
 }
 
 // Maximo devuelve la posición del mayor elemento del arreglo, o -1 si el el arreglo es
@@ -26,31 +28,70 @@ func Maximo(vector []int) int {
 }
 
 // Comparar compara dos arreglos de longitud especificada.
-// Devuelve -1 si el primer arreglo es menor que el segundo; 0 si son iguales; o 1 si el primero es el mayor.
-// Un arreglo es menor a otro cuando al compararlos elemento a elemento, el primer elemento en el que difieren
+// Devuelve -1 si el primer arreglo es menor que el segundo; 0 
+// si son iguales; o 1 si el primero es el mayor.
+// Un arreglo es menor a otro cuando al compararlos elemento 
+// a elemento, el primer elemento en el que difieren
 // no existe o es menor.
 func Comparar(vector1 []int, vector2 []int) int {
+	longitud1 := len(vector1)
+	longitud2 := len(vector2)
+
+	limite := longitud1
+	if longitud1 > longitud2{
+		limite = longitud2
+	}
+
+	for i := 0; i < limite; i++{
+		if vector1[i] < vector2[i]{
+			return -1
+		}else if vector1[i] > vector2[i]{
+			return 1
+		}
+	}
+	if longitud1 < longitud2{
+		return -1
+	}else if longitud1 > longitud2{
+		return 1
+	}
 	return 0
 }
 
 // Seleccion ordena el arreglo recibido mediante el algoritmo de selección.
 func Seleccion(vector []int) {
+	for i := 0 ; i < len(vector) -1 ; i ++{
+        minimo := i
+		for j := i+1; j < len(vector) ; j++{
+
+			if vector [j] < vector[minimo] {
+				minimo = j
+			}
+		}	
+		aux := vector[minimo]
+		vector[minimo] = vector[i]
+		vector[i] = aux
+}
 
 }
 
-// Suma devuelve la suma de los elementos de un arreglo. En caso de no tener elementos, debe devolver 0.
-// Esta función debe implementarse de forma RECURSIVA. Se puede usar una función auxiliar (que sea
+// Suma devuelve la suma de los elementos de un arreglo. 
+// En caso de no tener elementos, debe devolver 0.
+// Esta función debe implementarse de forma RECURSIVA. 
+// Se puede usar una función auxiliar (que sea
 // la recursiva).
 func Suma(vector []int) int {
-	if(len(vector) == 0){
-	return 0
+    if len(vector) == 0{
+		return 0
 	}
-	return Suma(vector[10]) 
+	return vector[0] + Suma(vector[1:])
 }
 
-// EsCadenaCapicua devuelve si la cadena es un palíndromo. Es decir, si se lee igual al derecho que al revés.
-// Esta función debe implementarse de forma RECURSIVA. Se puede usar una función auxiliar (que sea
+// EsCadenaCapicua devuelve si la cadena es un palíndromo. 
+// Es decir, si se lee igual al derecho que al revés.
+// Esta función debe implementarse de forma RECURSIVA. 
+// Se puede usar una función auxiliar (que sea
 // la recursiva).
 func EsCadenaCapicua(cadena string) bool {
+	
 	return false
 }
