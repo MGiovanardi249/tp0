f8e2a1010b13bab5f09cf626d5636d8ff1c15fe3
Maximo Giovanardi <mgiovanardi@fi.uba.ar>
2026-03-15 16:57:11 -0300

    Finalizar TP0: corrección de Seleccion para reutilizar Swap/Maximo y aplicar go fmt

--- ejercicios/tp0.go
+++ ejercicios/tp0.go
@@ -8,29 +8,31 @@ func Swap(x *int, y *int) {
 }
 
 // Maximo devuelve la posición del mayor elemento del arreglo, o -1 si el el arreglo es
-//  de largo 0. Si el máximo
+//
+//	de largo 0. Si el máximo
+//
 // elemento aparece más de una vez, se debe devolver la primera posición en que ocurre.
 func Maximo(vector []int) int {
-		if(len(vector) == 0 ){
-	return -1
+	if len(vector) == 0 {
+		return -1
 	}
 	posicionMaxima := 0
 	valorMaximo := vector[0]
-	
-	for i , v := range vector{
-	 	if valorMaximo < v {
+
+	for i, v := range vector {
+		if valorMaximo < v {
 			valorMaximo = v
 			posicionMaxima = i
 		}
-    
+
 	}
 	return posicionMaxima
 }
 
 // Comparar compara dos arreglos de longitud especificada.
-// Devuelve -1 si el primer arreglo es menor que el segundo; 0 
+// Devuelve -1 si el primer arreglo es menor que el segundo; 0
 // si son iguales; o 1 si el primero es el mayor.
-// Un arreglo es menor a otro cuando al compararlos elemento 
+// Un arreglo es menor a otro cuando al compararlos elemento
 // a elemento, el primer elemento en el que difieren
 // no existe o es menor.
 func Comparar(vector1 []int, vector2 []int) int {
@@ -38,20 +40,20 @@ func Comparar(vector1 []int, vector2 []int) int {
 	longitud2 := len(vector2)
 
 	limite := longitud1
-	if longitud1 > longitud2{
+	if longitud1 > longitud2 {
 		limite = longitud2
 	}
 
-	for i := 0; i < limite; i++{
-		if vector1[i] < vector2[i]{
+	for i := 0; i < limite; i++ {
+		if vector1[i] < vector2[i] {
 			return -1
-		}else if vector1[i] > vector2[i]{
+		} else if vector1[i] > vector2[i] {
 			return 1
 		}
 	}
-	if longitud1 < longitud2{
+	if longitud1 < longitud2 {
 		return -1
-	}else if longitud1 > longitud2{
+	} else if longitud1 > longitud2 {
 		return 1
 	}
 	return 0
@@ -59,47 +61,38 @@ func Comparar(vector1 []int, vector2 []int) int {
 
 // Seleccion ordena el arreglo recibido mediante el algoritmo de selección.
 func Seleccion(vector []int) {
-	for i := 0 ; i < len(vector) -1 ; i ++{
-        minimo := i
-		for j := i+1; j < len(vector) ; j++{
-
-			if vector [j] < vector[minimo] {
-				minimo = j
-			}
-		}	
-		aux := vector[minimo]
-		vector[minimo] = vector[i]
-		vector[i] = aux
+	for i := len(vector); i > 1; i-- {
+		Max := Maximo(vector[:i])
+		Swap(&vector[Max], &vector[i-1])
+	}
 }
 
-}
-
-// Suma devuelve la suma de los elementos de un arreglo. 
+// Suma devuelve la suma de los elementos de un arreglo.
 // En caso de no tener elementos, debe devolver 0.
-// Esta función debe implementarse de forma RECURSIVA. 
+// Esta función debe implementarse de forma RECURSIVA.
 // Se puede usar una función auxiliar (que sea
 // la recursiva).
 func Suma(vector []int) int {
-    if len(vector) == 0{
+	if len(vector) == 0 {
 		return 0
 	}
 	return vector[0] + Suma(vector[1:])
 }
 
-// EsCadenaCapicua devuelve si la cadena es un palíndromo. 
+// EsCadenaCapicua devuelve si la cadena es un palíndromo.
 // Es decir, si se lee igual al derecho que al revés.
-// Esta función debe implementarse de forma RECURSIVA. 
+// Esta función debe implementarse de forma RECURSIVA.
 // Se puede usar una función auxiliar (que sea
 // la recursiva).
 func EsCadenaCapicua(cadena string) bool {
-	if len(cadena) == 1 || len(cadena) == 0{
+	if len(cadena) == 1 || len(cadena) == 0 {
 		return true
 	}
 
-    if cadena[0] != cadena[len(cadena)-1]{
+	if cadena[0] != cadena[len(cadena)-1] {
 		return false
 	}
 
-	return EsCadenaCapicua(cadena[1:len(cadena)-1])
+	return EsCadenaCapicua(cadena[1 : len(cadena)-1])
 
 }
--- main.go
+++ main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"tp0/ejercicios"
+)
+
+func lectura(Archivo string) []int {
+	archivo, _ := os.Open(Archivo)
+	defer archivo.Close()
+
+	var numeros []int
+
+	scanner := bufio.NewScanner(archivo)
+
+	for scanner.Scan() {
+		texto := scanner.Text()
+		numero, _ := strconv.Atoi(texto)
+
+		numeros = append(numeros, numero)
+
+	}
+	return numeros
+}
+
+func main() {
+	v1 := lectura("archivo1.in")
+	v2 := lectura("archivo2.in")
+
+	if ejercicios.Comparar(v1, v2) == -1 {
+		ejercicios.Seleccion(v2)
+		for _, v := range v2 {
+			fmt.Println(v)
+		}
+	} else {
+		ejercicios.Seleccion(v1)
+		for _, v := range v1 {
+			fmt.Println(v)
+		}
+	}
+
+}
